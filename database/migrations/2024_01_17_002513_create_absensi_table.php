4076485e217266b248392e1791ab43f90de6a7d2
mrizkystr <muhamadrizkysatria23@gmail.com>
2024-01-17 08:54:12 +0700

    ini adalah update

---
+++ database/migrations/2024_01_17_002513_create_absensi_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('absensi', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('kelas');
+            $table->string('jurusan');
+            $table->enum('kehadiran',['hadir', 'izin'. 'sakit', 'alfa']);
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('absensi');
+    }
+};
